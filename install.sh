95ffb31b1ea61cdc3b20a45259b27fb1b7e30c2c
junavarg <64467772+junavarg@users.noreply.github.com>
2020-07-04 17:43:13 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Instala software soporte tarjeta joystick USB de Dragonrise 
-# desde fuentes
-# Requiere compilador golang instalado
 # Autor junavarg version 1 11/05/2020 
 
+mkdir ~/tmp-dragonrise
+cd ~/tmp-dragonrise
+rm *
 
-wget https://raw.githubusercontent.com/junavarg/dragonrise/master/31-dragonriseRPi.rules
+wget https://raw.githubusercontent.com/junavarg/dragonrise/master/bin/rpi/31-dragonriseRPi.rules
 sudo cp 31-dragonriseRPi.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 
-wget https://raw.githubusercontent.com/junavarg/dragonrise/master/dragonrise.go
-go get github.com/eclipse/paho.mqtt.golang 
-go build -ldflags "-s -w" dragonrise.go 
+wget https://raw.githubusercontent.com/junavarg/dragonrise/master/bin/rpi/dragonrise
+
  
 sudo cp dragonrise /usr/local/bin
 sudo chmod 777 /usr/local/bin/dragonrise
@@ -19,3 +19,6 @@ sudo chmod 777 /usr/local/bin/dragonrise
 # se crea el subdirectorio  para los ficheros de estado con propietario root y con permisos para todos
 sudo mkdir /var/lib/dragonrise
 sudo chmod 777 /var/lib/dragonrise
+
+cd ~
+rm -r ~/tmp-dragonrise
