33af55bbeadbc2c4169cf8494f473f4c32bc00b7
junavarg <64467772+junavarg@users.noreply.github.com>
2020-07-05 21:04:27 +0200

    Add files via upload

--- bin/rpi/dragonrise_service.sh
+++ bin/rpi/dragonrise_service.sh
@@ -5,4 +5,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "dragonrise service service started at ${DATE}" | systemd-cat -p info
 
 # se ejecuta dragonrise contra los 4 puertos USB externos de la Raspberry Pi
-dragonrise -mqpub=wss://mqttws.vigilanet.com:443/honduras6 /dev/dragonrise_2 /dev/dragonrise_3 /dev/dragonrise_4 /dev/dragonrise_5
+dragonrise -mqpub=tcp://test.mosquitto.org:1883/base_topic /dev/dragonrise_2 /dev/dragonrise_3 /dev/dragonrise_4 /dev/dragonrise_5
